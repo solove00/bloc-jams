b189f5a9d6e99de3113400da3ed23e930c6304be
Shan <anna.shan.ou@gmail.com>
2017-12-06 15:50:21 -0500

    assignment-22-DOMscript

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,37 +1,49 @@
 var pointsArray = document.getElementsByClassName('point');
 
-var animatePoints = function(points) {
+// homework 22
+var revealPoint = function(point) {
+    point.style.opacity = 1;
+    point.style.transform = "scaleX(1) translateY(0)";
+    point.style.msTransform = "scaleX(1) translateY(0)";
+    point.style.WebkitTransform = "scaleX(1) translateY(0)";
+  }
+var animatePoints = function(points){
+  forEach(points, revealPoints);
+}
+
+// checkpoint 21
+// var pointsArray = document.getElementsByClassName('point');
+// var animatePoints = function(points) {
+
+//   var revealFirstPoint = function() {
+//     points[0].style.opacity = 1;
+//     points[0].style.transform = "scaleX(1) translateY(0)";
+//     points[0].style.msTransform = "scaleX(1) translateY(0)";
+//     points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
+//   };
+//
+//   var revealSecondPoint = function() {
+//     points[1].style.opacity = 1;
+//     points[1].style.transform = "scaleX(1) translateY(0)";
+//     points[1].style.msTransform = "scaleX(1) translateY(0)";
+//     points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
+//   };
+//
+//   var revealThirdPoint = function() {
+//     points[2].style.opacity = 1;
+//     points[2].style.transform = "scaleX(1) translateY(0)";
+//     points[2].style.msTransform = "scaleX(1) translateY(0)";
+//     points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
+//   };
+//
+//   revealFirstPoint();
+//   revealSecondPoint();
+//   revealThirdPoint();
+//
+// };
 
 
-
-
-  var revealFirstPoint = function() {
-    points[0].style.opacity = 1;
-    points[0].style.transform = "scaleX(1) translateY(0)";
-    points[0].style.msTransform = "scaleX(1) translateY(0)";
-    points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-  };
-
-  var revealSecondPoint = function() {
-    points[1].style.opacity = 1;
-    points[1].style.transform = "scaleX(1) translateY(0)";
-    points[1].style.msTransform = "scaleX(1) translateY(0)";
-    points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-  };
-
-  var revealThirdPoint = function() {
-    points[2].style.opacity = 1;
-    points[2].style.transform = "scaleX(1) translateY(0)";
-    points[2].style.msTransform = "scaleX(1) translateY(0)";
-    points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-  };
-
-  revealFirstPoint();
-  revealSecondPoint();
-  revealThirdPoint();
-
-};
-
+// homeowrk 21
 // var revealPoint = function() {
 //   var points = document.getElementsByClassName('point');
 //   for (var i = 0; i < points.length; i++) {
@@ -42,7 +54,7 @@ var animatePoints = function(points) {
 //
 //   }
 // };
-
+//
 // revealPoint();
 
 window.onload = function() {
