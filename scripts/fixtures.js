e8eab863438e9e7153c252860fc1b81753035b22
Lin Cao <lin.cao@citrix.com>
2017-12-10 11:37:30 -0500

    checkpoint-32-BuzzLibrary
    add music files

--- scripts/fixtures.js
+++ scripts/fixtures.js
@@ -35,25 +35,11 @@ var albumMarconi = {
   label: 'EM',
   year: '1909',
   albumArtUrl: 'assets/images/album_covers/20.png',
-  songs: [{
-      title: 'Hello, Operator?',
-      duration: '1:01'
-    },
-    {
-      title: 'Ring, ring, ring',
-      duration: '5:01'
-    },
-    {
-      title: 'Fits in your pocket',
-      duration: '3:21'
-    },
-    {
-      title: 'Can you hear me now?',
-      duration: '3:14'
-    },
-    {
-      title: 'Wrong phone number',
-      duration: '2:15'
-    }
+  songs: [
+    { title: 'Blue', duration: '4:26', audioUrl: 'assets/music/blue' },
+    { title: 'Green', duration: '3:14', audioUrl: 'assets/music/green' },
+    { title: 'Red', duration: '5:01', audioUrl: 'assets/music/red' },
+    { title: 'Pink', duration: '3:21', audioUrl: 'assets/music/pink' },
+    { title: 'Magenta', duration: '2:15', audioUrl: 'assets/music/magenta' }  
   ]
 };
